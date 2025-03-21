6986543a1e18d15f4c3d11610d5f58f7fd8b2135
Arthur Danielson <arthurdanielson7@gmail.com>
2025-03-20 23:52:08 -0400

    20-03-2025 #2

--- 8-Sequelize/04_lendo_dados/index.js
+++ 8-Sequelize/04_lendo_dados/index.js
@@ -18,28 +18,6 @@ app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 
-app.get("/users/create", (req, res) => {
-  res.render("adduser");
-});
-
-app.post("/users/create", async (req, res) => {
-  const name = req.body.name;
-  const occupation = req.body.occupation;
-  let newsletter = req.body.newsletter;
-
-  console.log(req.body);
-
-  if (newsletter == "on") {
-    newsletter = true;
-  } else {
-    newsletter = false;
-  }
-
-  await User.create({ name, occupation, newsletter });
-
-  res.redirect("/");
-});
-
 app.get("/", async (req, res) => {
   const users = await User.findAll({ raw: true });
   res.render("home", { users: users });
--- 8-Sequelize/06_removendo_dados/index.js
+++ 8-Sequelize/06_removendo_dados/index.js
@@ -18,12 +18,42 @@ app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 
+app.get("/users/create", (req, res) => {
+  res.render("adduser");
+});
+
+app.post("/users/create", async (req, res) => {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let newsletter = req.body.newsletter;
+
+  console.log(req.body);
+
+  if (newsletter == "on") {
+    newsletter = true;
+  } else {
+    newsletter = false;
+  }
+
+  await User.create({ name, occupation, newsletter });
+
+  res.redirect("/");
+});
+
 app.get("/users/:id", async (req, res) => {
   const id = req.params.id;
 
   const user = await User.findOne({ raw: true, where: { id: id } });
 
-  res.render('userview', { user})
+  res.render("userview", { user });
+});
+
+app.post("/users/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await User.destroy({ where: { id: id } });
+
+  res.redirect("/");
 });
 
 app.get("/", async (req, res) => {
