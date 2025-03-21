936fb25ac09055efd547cafc733b082a91bfc7d5
Arthur Danielson <arthurdanielson7@gmail.com>
2025-03-21 10:00:34 -0400

    21-03-2025

--- 8-Sequelize/10_add_relacionado/index.js
+++ 8-Sequelize/10_add_relacionado/index.js
@@ -3,6 +3,7 @@ const exphbs = require("express-handlebars");
 const conn = require("./db/conn");
 
 const User = require("./models/User");
+const Address = require("./models/Address");
 
 const app = express();
 
@@ -17,6 +18,44 @@ app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
+
+app.get("/users/create", (req, res) => {
+  res.render("adduser");
+});
+
+app.post("/users/create", async (req, res) => {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let newsletter = req.body.newsletter;
+
+  console.log(req.body);
+
+  if (newsletter == "on") {
+    newsletter = true;
+  } else {
+    newsletter = false;
+  }
+
+  await User.create({ name, occupation, newsletter });
+
+  res.redirect("/");
+});
+
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const user = await User.findOne({ raw: true, where: { id: id } });
+
+  res.render("userview", { user });
+});
+
+app.post("/users/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await User.destroy({ where: { id: id } });
+
+  res.redirect("/");
+});
 
 // UPDATE
 app.get("/users/edit/:id", async (req, res) => {
@@ -49,6 +88,20 @@ app.post("/users/update", async (req, res) => {
   await User.update(userData, { where: { id: id } });
 
   res.redirect("/");
+});
+
+// 10_add_relacionamento
+app.post("/address/create", async (req, res) => {
+  const UserId = req.body.UserId;
+  const street = req.body.street;
+  const number = req.body.number;
+  const city = req.body.city;
+
+  const address = { UserId, street, number, city };
+
+  await Address.create(address);
+
+  res.redirect(`/users/edit/${UserId}`);
 });
 
 app.get("/", async (req, res) => {
