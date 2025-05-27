784c9f330ebaef866afae51c8b113764c06548ae
Arthur Danielson <arthurdanielson7@gmail.com>
2025-05-27 10:21:40 -0400

    27-05-2025

---
+++ 10-MongoDB/01 _ instalacao/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const conn = require("./db/conn");
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+// ready body
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.listen(3000);
